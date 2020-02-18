9e4606d1224152b7abf5503be4d100db880571d5
y3k <y3k00000@gmail.com>
2020-02-18 12:08:32 +0800

    init

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const TestComp = ({})=>{
+    return(<div>
+        
+    </div>)
+}
+
+ReactDom.render(<TestComp />, document.getElementById("react-container"));
